8ba461cad2c589e74df4c0aa0343a9f11f0f5f76
haley9477 <tea4829@gmail.com>
2023-05-03 19:12:13 +0800

    Initialize Project

---
+++ app.js
@@ -0,0 +1,38 @@
+// packages
+const express = require('express')
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+const app = express()
+
+// variables
+const port = 3000
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static engine
+app.use(express.static('public'))
+
+// routes setting 
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurants: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen the server
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`)
+})
